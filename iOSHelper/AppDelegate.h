1878840ceed2f0cc3fd093137c1b3b4230990bc2
pljhonglu <pljhonglu@126.com>
2014-07-22 20:42:25 +0800

    init

---
+++ iOSHelper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSHelper
+//
+//  Created by pljhonglu on 14-7-22.
+//  Copyright (c) 2014年 pljhonglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
