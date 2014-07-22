83f420a6538f043c14c2f92619a2825dd82d665e
pljhonglu <pljhonglu@126.com>
2014-07-23 06:58:10 +0800

    a

--- iOSHelper/AppDelegate.m
+++ iOSHelper/AppDelegate.m
@@ -16,6 +16,9 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    NSString *a = @"0.1333";
+    float b = [a floatValue];
     return YES;
 }
 
