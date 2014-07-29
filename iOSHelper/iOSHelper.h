ca2335174b60b5a211ad339c0fa4c812a8c7b0bd
pljhonglu <pljhonglu@126.com>
2014-07-29 21:15:48 +0800

    ioshelper.h

--- iOSHelper/iOSHelper.h
+++ iOSHelper/iOSHelper.h
@@ -12,12 +12,21 @@
 #error iOSHelper only support iOS 6.0 and later
 #endif
 
+
 #import "Macro.h"
 
+// Core Data
+#import "CoreDataManager.h"
+#import "NSManagedObject+helper.h"
+
+// helper
 #import "Lunar.h"
+#import "CookiesHelper.h"
 #import "ResourceHelper.h"
 #import "SFHFKeychainUtils.h"
 
+// UIKit
+#import "UIImage+ImageEffects.h"
 #import "UIAlertView+Helper.h"
 #import "UIButton+Helper.h"
 #import "UIControl+Helper.h"
@@ -27,9 +36,11 @@
 #import "UIViewController+Helper.h"
 #import "UIDevice+Helper.h"
 
-#import "CoreDataManager.h"
-#import "NSManagedObject+helper.h"
+// 这两个因为有覆盖父类的方法,所以根据需要在特定的类中加入
+// #import "UIScrollView+DeliveryEvent.h"
+// #import "UIScrollView+DeliveryAllEvents.h"
 
+// Foundation
 #import "NSArray+Helper.h"
 #import "NSData+Helper.h"
 #import "NSDate+Helper.h"
